f0c09be7c86b0eb768c9ee93ce833b328e575bd6
Shreyansh <shreyansh.pettswood@gmail.com>
2017-06-06 14:08:51 +0530

    Minor code changes

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ agenda.on('ready', () => {
 				});
 			}
 
-			if((message && message.text) || (postback.payload.includes("menu"))) {
+			if((message && message.text) || (postback && postback.payload.includes("menu"))) {
 				// Process the message here
 				let sessionId = session.init(sender);
 				let {context} = session.get(sessionId);
